89ace291b51fb01061c68299c417f343b523fa82
med ali may <may.mohamedali28@gmail.com>
2019-08-28 21:42:12 +0100

    chore(facebook-authentication.service): check if version changes

--- libs/core/backend/src/app/domain/authentication/social/facebook/facebook-authentication.service.ts
+++ libs/core/backend/src/app/domain/authentication/social/facebook/facebook-authentication.service.ts
@@ -80,7 +80,7 @@ export class FacebookAuthenticationService {
       return { token: result.access_token };
     } catch (error) {
       this.bffLoggerService.error(
-        'Error retrieving Facebook oAuth access token',
+        'error retrieving Facebook oAuth access token',
         error
       );
       throw new AppError(
