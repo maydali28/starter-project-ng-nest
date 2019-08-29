c5af3b18e77926d32fee611a929a5b238a46f040
dalimay <dalimay@localhost.localdomain>
2019-08-29 11:43:02 -0400

    chore(tools, libs package.json): add tools, update package.json for libs
    add build, test, check and publish script for libs|update package.json for libs to automate building
    using ng-packagr

---
+++ tools/scripts/libs-publish.js
@@ -0,0 +1,15 @@
+const { readdir } = require('fs-extra');
+const { pexec } = require('./utils');
+
+
+const publishLibs = async (dirs) => {
+    for (let dir of dirs) {
+        console.log(dir);
+        const publishResult = await pexec(`npm publish @starter-project-ng-nest/${dir}`);
+    }
+}
+
+(async () => {
+    const dirs = await readdir('./@starter-project-ng-nest');
+    await publishLibs(dirs);
+})()
